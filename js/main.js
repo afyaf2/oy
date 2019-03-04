26220a7e38107fb7c4b79505966a8bfc26257dbc
Adam Frej <awfrej@googlemail.com>
2019-03-04 13:39:25 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+  var typed3 = new Typed('#typed', {
+    strings: ['players', 'captains', 'leaders'],
+    typeSpeed: 100,
+    backSpeed: 10,
+    fadeOut: true,
+    showCursor: false,
+    loop: true
+  });
